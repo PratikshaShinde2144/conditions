69544a288d0de1eae7ea703f9c88be26a0e695f7
unknown <shindepratiksha1001@gmail.com>
2021-02-15 11:15:08 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './Pratiksha.css';
+function App() {
+  return (
+    <div>
+      <button className="myButton">Click Here To Hide/Show Div</button>
+    </div>
+  );
+}
+
+export default App;
